06963bc4ee79a5c380f762914210bef759d48d88
Ranjeet Ghatage <ranjeetrghatage@gmail.com>
2024-05-17 10:25:43 +0530

    Add Stream Logic Implemented

--- MauiApp3/MainPage.xaml.cs
+++ MauiApp3/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiApp3
+﻿using Microsoft.Maui.Controls;
+using System.Data.Common;
+
+namespace MauiApp3
 {
     public partial class MainPage : ContentPage
     {
@@ -29,52 +32,65 @@
 
         }
 
-
         private void Settings_Clicked(object sender, EventArgs e)
         {
+            var frame = new Frame
+            {
+                BorderColor = Colors.Transparent,
+                BackgroundColor = Colors.Transparent,
+                //Style = (Style)Application.Current.Resources["ImageFrame"],
+                
+            };
+
+            var grid = new Grid
+            {
+                Padding = new Thickness(-10, -10, -10, 0),
+                Margin = new Thickness(0, 1 == 0 ? 0 : -22, -15, 0)
+            };
+
+            var image = new Image
+            {
+                Source = "imageSource",
+                Aspect = Aspect.Fill,
+                Background = Colors.Black
+            };
+
+            var lastFrame = GalleryGrid.Children[GalleryGrid.Children.Count - 1] as Frame;
+            int CurrNumber = 0;
+            if (lastFrame != null && lastFrame.Content is Grid lastFrameGrid)
+            {
+                string lastFrameLabelText = lastFrameGrid.Children.OfType<Label>().FirstOrDefault().Text;
+                int firstSpaceIndex = lastFrameLabelText.IndexOf(" ");
+                CurrNumber = int.Parse(lastFrameLabelText.Substring(firstSpaceIndex + 1));
+            }
+
+            var label = new Label
+            {
+                Text = $"Stream {CurrNumber+1}",
+                TextColor = Colors.White,
+                VerticalOptions = LayoutOptions.End,
+                HorizontalOptions = LayoutOptions.Start,
+                Margin = new Thickness(20, 90, 10, 20)
+            };
+
+            grid.Children.Add(image);
+            grid.Children.Add(label);
+            frame.Content = grid;
+
+            GalleryGrid.SetRow(frame, 2);
+            GalleryGrid.SetColumn(frame, 0);
+
+            GalleryGrid.Children.Add(frame);
+
+
+
+          
+
+
+
+
 
         }
-
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    count++;
-
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
-
-        //private void OnAddStreamClicked(object sender, EventArgs e)
-        //{
-        //    var newFrame = new Frame
-        //    {
-        //        BackgroundColor = Colors.Black,
-        //        Content = new Grid
-        //        {
-        //            Children =
-        //            {
-        //                new Image
-        //                {
-        //                    Source = $"stream.jpg",
-        //                    Aspect = Aspect.AspectFill
-        //                },
-        //                new Label
-        //                {
-        //                    Text = $"Stream",
-        //                    TextColor = Colors.White,
-        //                    VerticalOptions = LayoutOptions.End,
-        //                    HorizontalOptions = LayoutOptions.Start,
-        //                    Margin = new Thickness(10)
-        //                }
-        //            }
-        //        }
-        //    };
-        //    StreamContainer.Children.Add(newFrame);
-        //}
-
 
 
 
